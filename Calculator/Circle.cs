dfe39bf5e87cf0bf3ad724658fc4bdb04a3ece66
Patryk <p-jeckowski@o2.pl>
2018-10-29 20:53:18 +0100

    merged implemented circle area calculation

---
+++ Calculator/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Circle : IShape
+    {
+        private double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        public Circle()
+        {
+            Console.WriteLine("GIB ME RADIUS!!!!");
+            _radius = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double CalculateArea()
+        {
+            return 2 * Math.PI * _radius;
+        }
+    }
+}
