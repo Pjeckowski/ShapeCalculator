b39b34befa2e1ca86e575fd100ffb6ae5c4c94c0
Patryk <p-jeckowski@o2.pl>
2018-10-29 20:33:45 +0100

    merged implemented square area calculation

---
+++ Calculator/IShapeCalculator.cs
@@ -0,0 +1,7 @@
+﻿namespace Calculator
+{
+    public interface IShapeCalculator
+    {
+        double CalculateArea(IShape shape);
+    }
+}
---
+++ Calculator/Square.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Square : IShape
+    {
+        private double _a;
+
+        public Square(double a)
+        {
+            _a = a;
+        }
+
+        public Square()
+        {
+            Console.WriteLine("Podaj bok a:");
+            _a = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public double CalculateArea()
+        {
+            return _a * _a;
+        }
+    }
+}
